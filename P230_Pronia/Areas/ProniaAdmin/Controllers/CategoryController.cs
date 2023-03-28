0aa434662dec08dd3d5711ab74c46d1e7d616882
elchinma <elchinma@code.edu.az>
2023-03-28 22:20:49 +0400

    S

---
+++ P230_Pronia/Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P230_Pronia.DAL;
+using P230_Pronia.Entities;
+using P230_Pronia.Migrations;
+using System.Reflection;
+
+namespace P230_Pronia.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class CategoryController : Controller
+    {
+        private readonly ProniaDbContext _context;
+        private IEnumerable<object> dbSetEntities;
+
+        public CategoryController(ProniaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories.AsEnumerable();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Creates(Category newCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Name", "You cannot duplicate category name");
+                return View();
+            }
+            _context.Categories.Add(newCategory);
+            _context.SaveChanges();
+            Console.WriteLine("Hello World");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == 0) return NotFound();
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category is null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(int id, Category edited)
+        {
+            if (id != edited.Id) return BadRequest();
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category is null) return NotFound();
+            bool duplicate = _context.Categories.Any(c => c.Name == edited.Name);
+            if (duplicate)
+            {
+                ModelState.AddModelError("", "You cannot duplicate category name");
+                return View();
+            }
+            category.Name = edited.Name;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        [HttpPost]
+        [ActionName("Delete")]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult DeleteEntity(string dbSetName, string id)
+        {
+            var viewModel = new DataDeleteViewModel();
+            viewModel.DbSetName = dbSetName;
+            viewModel.Id = id;
+            viewModel.Object = GetEntityFromDbSet(dbSetName, id, out var dbContext, out var entityType, out var relationships);
+            if (viewModel.Object == null) return NotFound();
+
+            return View(viewModel);
+        }
+
+        private object GetEntityFromDbSet(string dbSetName, string id, out object dbContext, out object entityType, out object relationships)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> DeleteEntityPost([FromForm] DataDeleteViewModel viewModel)
+        {
+            foreach (var dbSetEntity in dbSetEntities.Where(db =>
+            {
+                return db.Name.ToLowerInvariant()
+                       == viewModel.DbSetName.ToLowerInvariant();
+            }))
+            {
+                foreach (var dbSetProperty in dbSetEntity.DbContextType.GetProperties())
+                {
+                    if (dbSetProperty.PropertyType.IsGenericType && dbSetProperty.PropertyType.Name.StartsWith("DbSet") && dbSetProperty.Name.ToLowerInvariant() == viewModel.DbSetName.ToLowerInvariant())
+                    {
+                        var dbContextObject = (DbContext)this.HttpContext.RequestServices.GetRequiredService(dbSetEntity.DbContextType);
+                        var dbSetValue = dbSetProperty.GetValue(dbContextObject);
+
+                        var primaryKey = dbContextObject.Model.FindEntityType(dbSetProperty.PropertyType.GetGenericArguments()[0]).FindPrimaryKey();
+                        var clrType = primaryKey.Properties[0].ClrType;
+
+                        object convertedPrimaryKey = viewModel.Id;
+                        if (clrType == typeof(Guid))
+                        {
+                            convertedPrimaryKey = Guid.Parse(viewModel.Id);
+                        }
+                        else if (clrType == typeof(int))
+                        {
+                            convertedPrimaryKey = int.Parse(viewModel.Id);
+                        }
+                        else if (clrType == typeof(Int64))
+                        {
+                            convertedPrimaryKey = Int64.Parse(viewModel.Id);
+                        }
+
+                        var entityToDelete = dbSetValue.GetType().InvokeMember("Find", BindingFlags.InvokeMethod, null, dbSetValue, args: new object[] { convertedPrimaryKey });
+                        dbSetValue.GetType().InvokeMember("Remove", BindingFlags.InvokeMethod, null, dbSetValue, args: new object[] { entityToDelete });
+
+                        await dbContextObject.SaveChangesAsync();
+
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ P230_Pronia/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P230_Pronia.Entities
+{
+    public class Category : BaseEntity
+    {
+        [Required(ErrorMessage="Zehmet olmasa inputu doldurun")]
+        public string Name { get; set; }
+        public List<PlantCategory> PlantCategories{ get; set; }
+    }
+}
